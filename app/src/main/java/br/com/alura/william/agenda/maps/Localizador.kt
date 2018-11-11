9351cb167f6f27c4b886c91da0ab900ff02d8952
William França <williamucep@hotmail.com>
2018-11-11 15:56:27 -0300

    Parte III: Aula 3/3

--- app/src/main/java/br/com/alura/william/agenda/maps/Localizador.kt
+++ app/src/main/java/br/com/alura/william/agenda/maps/Localizador.kt
@@ -1,21 +1,47 @@
 package br.com.alura.william.agenda.maps
 
-//class Localizador(
-//        var context: Context,
-//        var googleMap: GoogleMap
-//
-//) : GoogleApiClient.ConnectionCallbacks {
+import android.content.Context
+import android.location.Location
+import android.os.Bundle
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 
-//    val cliente = GoogleApiClient.Builder(context)
-//            .addApi(LocationServices.API)
-//            .addConnectionCallbacks(this)
-//            .build()
+class Localizador(var context: Context, googleMapa: GoogleMap) : GoogleApiClient.ConnectionCallbacks, com.google.android.gms.location.LocationListener {
 
-//    override fun onConnected(p0: Bundle?) {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-//
-//    override fun onConnectionSuspended(p0: Int) {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-//}
+    var googleMap: GoogleMap
+
+    var googleApiClient: GoogleApiClient = GoogleApiClient.Builder(context)
+            .addApi(LocationServices.API)
+            .addConnectionCallbacks(this)
+            .build()
+
+    init {
+        googleApiClient.connect()
+        googleMap = googleMapa
+    }
+
+    override fun onConnected(bundle: Bundle?) {
+        val locationRequest = LocationRequest()
+        locationRequest.smallestDisplacement = 50F
+        locationRequest.interval = 1000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                googleApiClient,
+                locationRequest,
+                this
+        )
+    }
+
+    override fun onConnectionSuspended(i: Int) {}
+
+    override fun onLocationChanged(location: Location?) {
+        val coordenada = LatLng(location?.latitude!!, location.longitude)
+        val cameraUpdate = CameraUpdateFactory.newLatLng(coordenada)
+        googleMap.moveCamera(cameraUpdate)
+    }
+}
--- app/src/main/java/br/com/alura/william/agenda/maps/MapaActivity.kt
+++ app/src/main/java/br/com/alura/william/agenda/maps/MapaActivity.kt
@@ -1,10 +1,16 @@
 package br.com.alura.william.agenda.maps
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import br.com.alura.william.agenda.R
 
 class MapaActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSOES = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,15 @@ class MapaActivity : AppCompatActivity() {
                 .beginTransaction()
                 .replace(R.id.fl_mapa, MapaFragment())
                 .commit()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val permissoes = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                requestPermissions(permissoes, REQUEST_PERMISSOES)
+            }
+        }
     }
 }
